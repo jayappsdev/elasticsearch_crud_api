bc52da4650d93652cf77e43252dafe80069a6718
jayappsdev <jayappsdev@gmail.com>
2023-12-07 18:30:01 +0100

    first commit

---
+++ Services/IElasticsearchService.cs
@@ -0,0 +1,11 @@
+﻿namespace ESCrud.Services
+{
+    public interface IElasticsearchService<T>
+    {
+        Task<string> CreateDocumentAsync(T document);
+        Task<T> GetDocumentAsync(int id);
+        Task<IEnumerable<T>> GetAllDocuments();
+        Task<string> UpdateDocumentAsync(T document);
+        Task<string> DeleteDocumentAsync(int id);
+    }
+}
